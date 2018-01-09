c5b5076d2b6a9f36f260462f969212650f75e86c
dzianismakeichyk <dzianis.makeichyk@gmail.com>
2018-01-09 19:33:24 +0100

    before pull

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,2 +1,4 @@
 console.log('hello2')
 
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImluZGV4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQSIsImZpbGUiOiJzY3JpcHRzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2hlbGxvMicpXG5cbiJdfQ==
