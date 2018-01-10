2982a2d73f5e76d4b2dd7b88c92f501d23da86d5
dzianis <dzianis.makeichyk@gmail.com>
2018-01-10 12:11:02 +0100

    320 normal menu

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     // Menu
-    console.log('hello2')
     $('.nav-button').click(function () {
-        $('.burger').toggleClass('active');
-        $('.screen-navigation').toggleClass('open');
+        $('.menu-container').toggleClass('is-active');
     });
 });
--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,10 +1,8 @@
 $(document).ready(function () {
     // Menu
-    console.log('hello2')
     $('.nav-button').click(function () {
-        $('.burger').toggleClass('active');
-        $('.screen-navigation').toggleClass('open');
+        $('.menu-container').toggleClass('is-active');
     });
 });
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRzLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcbiAgICAvLyBNZW51XG4gICAgY29uc29sZS5sb2coJ2hlbGxvMicpXG4gICAgJCgnLm5hdi1idXR0b24nKS5jbGljayhmdW5jdGlvbiAoKSB7XG4gICAgICAgICQoJy5idXJnZXInKS50b2dnbGVDbGFzcygnYWN0aXZlJyk7XG4gICAgICAgICQoJy5zY3JlZW4tbmF2aWdhdGlvbicpLnRvZ2dsZUNsYXNzKCdvcGVuJyk7XG4gICAgfSk7XG59KTtcbiJdLCJmaWxlIjoic2NyaXB0cy5qcyJ9
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRzLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uICgpIHtcbiAgICAvLyBNZW51XG4gICAgJCgnLm5hdi1idXR0b24nKS5jbGljayhmdW5jdGlvbiAoKSB7XG4gICAgICAgICQoJy5tZW51LWNvbnRhaW5lcicpLnRvZ2dsZUNsYXNzKCdpcy1hY3RpdmUnKTtcbiAgICB9KTtcbn0pO1xuIl0sImZpbGUiOiJzY3JpcHRzLmpzIn0=
